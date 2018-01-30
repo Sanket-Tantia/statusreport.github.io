784b8d412aabceb296b7002251b7d0835b4eb18c
Sanket Tantia <22017124+Sanket-Tantia@users.noreply.github.com>
2018-01-30 10:58:40 +0530

    qwqw

--- insert.php
+++ insert.php
@@ -10,7 +10,7 @@ if ($con->connect_error) {
 echo "Connected successfully";
 
 $sql = "INSERT INTO tbl (NAME, EID)
-        VALUES ('".$_GET["name"]."','".$_GET["eid"]."')";
+        VALUES ('".$_POST["name"]."','".$_POST["eid"]."')";
 if($con->query($sql)===TRUE)
 {
 echo "new record inserted successfully";
