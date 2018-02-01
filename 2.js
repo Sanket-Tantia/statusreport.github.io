78a9f036d7675566af4846e6e365a4e1b867c512
Sanket Tantia <22017124+Sanket-Tantia@users.noreply.github.com>
2018-02-01 19:41:42 +0530

    final changes - finally

---
+++ 2.js
@@ -0,0 +1,102 @@
+var formatting = function(a){
+  b=a.split('/')
+  if (parseInt(b[0])<10){
+    b[0]='0'+b[0];
+  }
+  if (parseInt(b[1])<10){
+    b[1]='0'+b[1];
+  }
+  a=b[0]+'/'+b[1]+'/'+b[2];
+  console.log(a);
+    return a;
+}
+
+var today = "";
+if (new Date().getMonth() < 10)  { today += '0' + (parseInt(new Date().getMonth())+1) }
+today+="/";
+if (new Date().getDate() < 10)  { today += '0' + (parseInt(new Date().getMonth())) }
+today+="/";
+today += new Date().getFullYear();
+
+var datepicker = new Datepickk();
+var selected=today;
+
+
+$('#jan').on('click', function() {
+  datepicker.disabledDays = [0,6];
+  datepicker.maxSelections = 1;
+  datepicker.minDate = new Date(2018,0,15);
+  datepicker.maxDate = new Date(2018,0,31);
+    datepicker.show();
+    datepicker.onSelect = function(checked){
+    var state = (checked)?'selected':'unselected';
+    if (state === 'selected'){
+      selected= this.toLocaleDateString();
+      selected= formatting(selected);
+      sessionStorage.setItem("selected_date", selected);
+      console.log(selected);
+      window.location.href = "3.html";
+    }
+  };
+
+  });
+
+$('#feb').on('click', function() {
+  datepicker.disabledDays = [0,6];
+  datepicker.maxSelections = 1;
+  datepicker.minDate = new Date(2018,1,1);
+  datepicker.maxDate = new Date(2018,1,28);
+    datepicker.show();
+    datepicker.onSelect = function(checked){
+    var state = (checked)?'selected':'unselected';
+    if (state === 'selected'){
+      selected= this.toLocaleDateString();
+      selected= formatting(selected);
+      sessionStorage.setItem("selected_date", selected);
+      console.log(selected);
+      window.location.href = "3.html";
+    }
+  };
+
+  });
+
+
+$('#mar').on('click', function() {
+  datepicker.disabledDays = [0,6];
+  datepicker.maxSelections = 1;
+  datepicker.minDate = new Date(2018,2,1);
+  datepicker.maxDate = new Date(2018,2,31);
+    datepicker.show();
+    datepicker.onSelect = function(checked){
+    var state = (checked)?'selected':'unselected';
+
+    if (state === 'selected'){
+      selected= this.toLocaleDateString();
+      selected= formatting(selected);
+      sessionStorage.setItem("selected_date", selected);
+      console.log(selected);
+      window.location.href = "3.html";
+    }
+  };
+
+  });
+
+
+  $('#apr').on('click', function() {
+  datepicker.disabledDays = [0,6];
+  datepicker.maxSelections = 1;
+  datepicker.minDate = new Date(2018,3,1);
+  datepicker.maxDate = new Date(2018,3,30);
+    datepicker.show();
+    datepicker.onSelect = function(checked){
+    var state = (checked)?'selected':'unselected';
+    if (state === 'selected'){
+      selected= this.toLocaleDateString();
+      selected= formatting(selected);
+      sessionStorage.setItem("selected_date", selected);
+      console.log(selected);
+      window.location.href = "3.html";
+    }
+  };
+
+});
